23ba46a9e49857136f7b0eaf16e0ec70a4bee73b
Gwilherm <d.gwilherm@yahoo.com>
2019-11-02 23:51:34 +0100

    Added chatterbot

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Pour le moment c'est un bot en Python d'une simplicité naïve. Pour l'utiliser 
 * Un Python 3 récent
 * Le module crontab
 * Le module discord.py
+* Le module chatterbot
+* Le module chatterbot_corpus
 
 Pour fonctionner, le script a besoin que le token d'authentification du bot soit passé dans
 la variable d'environnement `FIRBOT_TOKEN`. Il chargera alors un fichier `cron.tab` dans son
@@ -17,4 +19,10 @@ Exemple :
 
 ```
 * * * * *, 000000000000000000, Le chantier avance :ok_hand:
+```
+
+Il est possible de lui apprendre des dialogues avec la commande ``!learn``
+Exemple:
+```
+!learn "phrase 1" "phrase 2" "phrase 3"
 ```
--- firbot.py
+++ firbot.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python 
 #-*- coding: utf-8 -*-
 
+import re
+import random
 import asyncio
 import os
 import traceback
 from pathlib import Path
 
+import sqlite3
+
 import discord
+from discord.ext import commands
 from crontab import CronTab
 
-client = discord.Client()
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+BOT_PREFIX = ('?', '!')
+client = commands.Bot(command_prefix=BOT_PREFIX)
+
 running_tasks = []
+
+noob = False
+if not os.path.isfile("db.sqlite3"):
+    noob = True
+
+chatbot = ChatBot('Firbot')
+
+if noob:
+    trainer = ChatterBotCorpusTrainer(chatbot)
+    trainer.train("chatterbot.corpus.french")
+
+trainer = ListTrainer(chatbot)
 
 async def send_message(interval, channel, text):
     await client.wait_until_ready()
@@ -66,5 +89,30 @@ async def on_ready():
             print('Could not schedule task:')
             traceback.format_exc()
             raise
+
+@client.command(pass_context=True)
+async def learn(ctx, *args):
+    print(f"Handle learn command: {args}")
+    trainer.train(args)
+
+@client.event
+@asyncio.coroutine
+def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    print(f"Received: '{message.content}'")
+    if message.content.startswith(BOT_PREFIX):
+        # Pass on to rest of the client commands
+        print("Process command")
+        yield from client.process_commands(message)
+    else:
+        if client.user.id in list(map(lambda m: m.id, message.mentions)):
+            cleanedMsg = re.sub('<@.*?>', '', message.content).strip()
+            answer = chatbot.get_response(cleanedMsg)
+            yield from message.channel.trigger_typing()
+            yield from asyncio.sleep(random.randint(1,3))
+            yield from message.channel.send(answer)
 
 client.run(os.environ['FIRBOT_TOKEN'])
