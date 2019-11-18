00643247ea11a87df5312757b9b97ec8bdfe64e4
Gwilherm <d.gwilherm@yahoo.com>
2019-11-18 23:14:48 +0100

    Add quit command

--- firbot.py
+++ firbot.py
@@ -14,6 +14,8 @@ from crontab import CronTab
 import firbot.cherchord
 import firbot.data
 
+ADMIN_ROLE = "FirbotMaster"
+
 bot = discord.ext.commands.Bot(command_prefix='!')
 running_tasks = []
 
@@ -79,5 +81,13 @@ async def cherchord(context, *args):
     for line in firbot.cherchord.cherchord(*args):
         await context.send(line)
 
+@bot.command()
+async def quit(context):
+    print(f"Handle quit")
+    if ADMIN_ROLE in list(map(lambda r: r.name, context.author.roles)):
+        await context.channel.send("Bye.")
+        await bot.logout()
+    else:
+        print(f'Member {context.author.name} not allowed to run this command.')
 
 bot.run(os.environ['FIRBOT_TOKEN'])
